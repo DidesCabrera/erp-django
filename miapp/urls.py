c79abd5cafc7df85ade155aa39cb16eb89a9819d
Felipe Dides <felipe@local.dev>
2026-02-28 01:43:19 -0300

    Add root redirect

--- miapp/urls.py
+++ miapp/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
+def root_redirect(request):
+    return redirect("/accounts/login/")
+
 urlpatterns = [
+    path("", root_redirect),
     path("admin/", admin.site.urls),
 
     # Auth system
