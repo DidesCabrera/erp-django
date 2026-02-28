109939fa08ea0b2bb1c39b03b7e45c94f73a85f6
Felipe Dides <felipe@local.dev>
2026-02-28 02:13:52 -0300

    Add landing page V1

--- miapp/settings.py
+++ miapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'notas.apps.NotasConfig',
     'accounts',
+    'core',
 ]
 
 MIDDLEWARE = [
--- miapp/urls.py
+++ miapp/urls.py
@@ -5,7 +5,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", lambda request: redirect("accounts/login/")),
+    
     path("admin/", admin.site.urls),
+    
+    # Landing pública
+    path("", include("core.urls")),
 
     # Auth system
     path("accounts/", include("django.contrib.auth.urls")),
