e71e416bc95454494372e7c0d12b64324e15e965
Felipe Dides <felipe@local.dev>
2026-02-27 23:20:56 -0300

    Configure dynamic database for production

--- miapp/settings.py
+++ miapp/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'miapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
