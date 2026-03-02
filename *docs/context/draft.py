b0cae65ecc9631531cc083a986beaa85294706d0
Felipe Dides <felipe@local.dev>
2026-03-02 14:39:59 -0300

    Change in manage.py

--- *docs/context/draft.py
+++ *docs/context/draft.py
@@ -1,31 +1,174 @@
-Tengo esta parte de html
+import os
+from pathlib import Path
+import dj_database_url
 
-<a href="{% url 'dailyplan_list' %}"
-    class="nav-tab nav-tab--personal tab-list {% if request.resolver_match.url_name == 'dailyplan_list' %}is-active{% endif %}">
-    <i data-lucide="bookmark" class="sidebar-leading-icon"></i>
-    <span class="nav-text">
-        Mi Libreria
-        <div class="nav-indicator"></div>
-    </span>    
-</a>
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-y esta estilizado asi...
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
 
 
-.nav-tab {
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
-  text-decoration: none;
-  padding: 3px 8px;
-  margin-top: 5px;
-  color: inherit;
-  cursor: pointer;
-  user-select: none;
+
+#==============================
+#==============================
+#==============================
+
+
+
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'notas.apps.NotasConfig',
+    'accounts',
+    'core',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+ROOT_URLCONF = 'miapp.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'notas.context_processors.user_weight',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'miapp.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
-.nav-tab.tab-list {
-  margin-top: 2px;
-}
 
-Me gustaría que el icono y el span esten uno al lado de otro ambos al lado izquierdfo.
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+
+AUTH_PASSWORD_VALIDATORS = []
+
+"""
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+"""
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/app/dailyplans/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "noreply@notas.com"
+
+
+
+
+------
+dev
+
+
+
+from .base import *
+
+SECRET_KEY = "dev-secret-key-local-only"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+print("🚀 RUNNING IN DEV MODE")
+
+-----
+
+
+
+from .base import *
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "www.myscoope.com,myscoope.com,my-scoope.onrender.com"
+).split(",")
